40531addd47e5f36467a4b69736936c19d5ede6e
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-03 01:27:14 +0400

    Разработан интерфейс IRemind позволяющий устанавливать и считывать дату-время напоминания о событии.

---
+++ dotNet module/Task_2/IRemind.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+  interface IRemind
+  {
+    public void getDateTimeRemind();
+    public DateTime setDateTimeRemind(); 
+  }
+}
