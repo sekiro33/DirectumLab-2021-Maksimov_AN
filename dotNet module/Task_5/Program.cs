a39d213a3afd528a7442ebc714929a0f083060a8
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-11 09:28:58 +0400

    Создан класс StringValue. Класс доработан по заданию, теперь проверка эквивалентности двух экземпляров этого класса работает правильно.

---
+++ dotNet module/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_5
+{
+  /// <summary>
+  /// Program.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// Точка входа в программу.
+    /// </summary>
+    /// <param name="args">Аргументы.</param>
+    private static void Main(string[] args)
+    { 
+      Console.WriteLine(new StringValue("а").Equals(new StringValue("а")));
+      Console.ReadKey();
+    }
+  }
+}
---
+++ dotNet module/Task_5/StringValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+  /// <summary>
+  /// Обертка класса строки.
+  /// </summary>
+  public class StringValue
+  {
+    /// <summary>
+    /// Строка.
+    /// </summary>
+    public string Value { get; private set; }
+
+    /// <summary>
+    /// Создать обертку класса строки.
+    /// </summary>
+    /// <param name="value">Строка.</param>
+    public StringValue(string value)
+    {
+      this.Value = value;
+    }
+
+    /// <summary>
+    /// Определяет, имеют ли два объекта StringValue одинаковое значение.
+    /// </summary>
+    /// <param name="obj">Объект для сравнения с текущим StringValue.</param>
+    /// <returns>true если obj представленный как StringValue и если его значение совпадает с этим экземпляром; иначе - false.</returns>
+    public override bool Equals(object obj)
+    {
+      StringValue castObj = (StringValue)obj;
+      return this.Value.Equals(castObj.Value);
+    }
+  }
+}
