9493be28540117da9e2fbe6b71c874e8ad673538
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-15 10:47:01 +0400

    Создан проект и оконная форма. Добавлен обработчик для кнопки "Обзор".

---
+++ dotNet module/Task_7/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace Task_7
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    /// <summary>
+    /// Обработчик нажатия на кнопку "Обзор".
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+      var openFileDialog = new OpenFileDialog();
+      openFileDialog.Filter = "Текстовый файл, сжатый по методу GZIP|*.gz";
+
+      if (openFileDialog.ShowDialog() == true)
+      {
+        AbsolutePathTextField.Text = openFileDialog.FileName;
+      }
+    }
+  }
+}
