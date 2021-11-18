9445e665ac0da898e31d478f88be8ad26ffc3cc6
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-18 20:46:55 +0400

    Доработана процедура загрузки данных из текстового файла, сжатого по методу GZIP: добавлена обработка исключений. Разработана функция, которая находит максимум из значений поступаемых на вход функции. Проверка типов выполняется на этапе компиляции. Разработана процедура для перебора элементов generic-списков и generic-словарей. Разработан класс, который используется для перебора строк текстового файла в цикле foreach.

--- dotNet module/Task_2/Program.cs
+++ dotNet module/Task_2/Program.cs
@@ -2,8 +2,15 @@
 
 namespace Task_2
 {
+  /// <summary>
+  /// Program.
+  /// </summary>
   public class Program
   {
+    /// <summary>
+    /// Точка входа в программу.
+    /// </summary>
+    /// <param name="args">Аргументы.</param>
     public static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
