c38f153dd766ae5cc0c071b4d0a890e736c18a22
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-18 23:03:50 +0400

    Создан проект для выполнения 10 практического задания.

---
+++ dotNet module/Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_10
+{
+  /// <summary>
+  /// Program.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// Точка входа в программу.
+    /// </summary>
+    /// <param name="args">Параметры.</param>
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
