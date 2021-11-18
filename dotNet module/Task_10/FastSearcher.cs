d62af636114b3a2db1a18b2af3190511139c4f8d
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-18 23:12:08 +0400

    Создан класс FastSearcher. Описаны свойства, отвечающие за максимальное количество параллельных задач и минимальное количество значений обрабатываемых в потоке.

---
+++ dotNet module/Task_10/FastSearcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+  /// <summary>
+  /// Класс для реализации параллельного поиска.
+  /// </summary>
+  public class FastSearcher
+  {
+    private int maxParallelTasks;
+
+    private int minProcessedValues;
+
+    /// <summary>
+    /// Максимальное количество параллельных задач.
+    /// </summary>
+    public int MaxParrallelTasks
+    {
+      get => this.maxParallelTasks;
+
+      set
+      {
+        if (value >= 1)
+          this.maxParallelTasks = value;
+        else
+          throw new ArgumentException(value.ToString());
+      }
+    }
+
+    /// <summary>
+    /// Минимальное количество значений обрабатываемых в одном потоке.
+    /// </summary>
+    public int MinProcessedValues
+    {
+      get => this.minProcessedValues;
+
+      set
+      {
+        if (value >= 1)
+          this.minProcessedValues = value;
+        else
+          throw new ArgumentException(value.ToString());
+      }
+    }
+  }
+}
