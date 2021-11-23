c6c2f0e8e601d5e42a3c94787e1c52268db965fc
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-23 12:26:50 +0400

    Небольшие правки.

--- dotNet module/Task_9/StreamReaderEnumerable.cs
+++ dotNet module/Task_9/StreamReaderEnumerable.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Task_9
 {
   public class StreamReaderEnumerable : IEnumerable<string>
   {
     private string filePath;
+
     public StreamReaderEnumerable(string filePath)
     {
       this.filePath = filePath;
@@ -21,93 +20,6 @@ namespace Task_9
     IEnumerator IEnumerable.GetEnumerator()
     {
       return this.GetEnumerator();
-    }
-  }
-
-  public class StreamReaderEnumerator : IEnumerator<string>
-  {
-    private StreamReader sr;
-
-    public StreamReaderEnumerator(string filePath)
-    {
-      this.sr = new StreamReader(filePath);
-    }
-
-    private string current;
-
-    public string Current
-    {
-      get
-      {
-        if (this.sr == null || this.current == null)
-        {
-          throw new InvalidOperationException();
-        }
-
-        return this.current;
-      }
-    }
-
-    object IEnumerator.Current
-    {
-      get { return this.Current; }
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <returns></returns>
-    public bool MoveNext()
-    {
-      this.current = this.sr.ReadLine();
-      if (this.current == null)
-        return false;
-      return true;
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public void Reset()
-    {
-      this.sr.DiscardBufferedData();
-      this.sr.BaseStream.Seek(0, SeekOrigin.Begin);
-      this.current = null;
-    }
-
-    private bool disposedValue = false;
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public void Dispose()
-    {
-      this.Dispose(disposing: true);
-      GC.SuppressFinalize(this);
-    }
-
-    /// <summary>
-    /// Освобождение ресурсов.
-    /// </summary>
-    /// <param name="disposing"></param>
-    protected virtual void Dispose(bool disposing)
-    {
-      if (!this.disposedValue)
-      {
-        this.current = null;
-        if (this.sr != null)
-        {
-          this.sr.Close();
-          this.sr.Dispose();
-        }
-      }
-
-      this.disposedValue = true;
-    }
-
-    ~StreamReaderEnumerator()
-    {
-      this.Dispose(disposing: false);
     }
   }
 }
