014cd8f6ca32f8544eee57cfa62dbe83e42d18ba
sekiro33 <aleksejmakismov1@gmail.com>
2021-11-23 15:52:23 +0400

    Выполнены первое и третье задания.

---
+++ dotNet module/Task_12/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+  public class Person
+  {
+    private string name;
+    private int age;
+
+    [Obsolete]
+    public string Name 
+    { 
+      get => this.name; 
+      set => this.name = value; 
+    }
+
+    [Display(Name = "How much cost book")]
+    public int Age
+    {
+      get => this.age; 
+      
+      set
+      {
+        if (this.age >= 0)
+          this.age = value;
+      }
+    }
+  }
+}
---
+++ dotNet module/Task_12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_12
+{
+  /// <summary>
+  /// Program.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// Точка входа в программу.
+    /// </summary>
+    /// <param name="args">Параметры.</param>
+    public static void Main(string[] args)
+    {
+      var person = new Person();
+      PrintCollection(GetAllReadWritePropertiesWithIgnore(person));
+    }
+
+    /// <summary>
+    /// Получить список всех read-write свойств объекта.
+    /// </summary>
+    /// <param name="obj">Объект в котором читаются свойства.</param>
+    /// <returns>Список всех read-write свойств объекта</returns>
+    private static Dictionary<string, string> GetAllReadWriteProperties(object obj)
+    {
+      return obj.GetType().GetProperties()
+        .ToDictionary(p => p.Name, p => p.PropertyType.Name);
+    }
+
+    /// <summary>
+    /// Получить список всех read-write свойств объекта, кроме свойств с аттрибутом Obsolete.
+    /// </summary>
+    /// <param name="obj">Объект в котором читаются свойства.</param>
+    /// <returns>Список всех read-write свойств объекта, кроме свойств с аттрибутом Obsolete</returns>
+    private static Dictionary<string, string> GetAllReadWritePropertiesWithIgnore(object obj)
+    { 
+      return obj.GetType().GetProperties()
+        .Where(p => p.GetCustomAttributes(true).Any(attr => !(attr is ObsoleteAttribute)))
+        .ToDictionary(p => p.Name, p => p.PropertyType.Name);
+    }
+
+    private static void PrintCollection(IEnumerable collection)
+    {
+      foreach (var item in collection)
+        Console.WriteLine(item);
+    }
+  }
+}
