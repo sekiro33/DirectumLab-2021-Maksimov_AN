d74abfb31edbc6e5290e3943ba1f831f78282d3d
sekiro33 <63190662+sekiro33@users.noreply.github.com>
2021-12-22 22:35:57 +0400

    Исправил стиль написания

--- SQL/Блок_4.sql
+++ SQL/Блок_4.sql
@@ -1,126 +1,100 @@
 /*1. Отобразить названия организации всех покупателей из Торонто*/
-SELECT
-	SalesLT.Customer.CompanyName
-FROM 
-	SalesLT.Customer 
-JOIN
-	SalesLT.CustomerAddress
-ON 
-	SalesLT.Customer.CustomerID = SalesLT.CustomerAddress.CustomerID
-JOIN 
-	SalesLT.Address
-ON 
-	SalesLT.CustomerAddress.AddressID = SalesLT.Address.AddressID
-WHERE 
-	SalesLT.Address.City = 'Toronto';
+select
+  SalesLT.Customer.CompanyName
+from 
+  SalesLT.Customer 
+  join SalesLT.CustomerAddress on SalesLT.Customer.CustomerID = SalesLT.CustomerAddress.CustomerID
+  join SalesLT.Address on SalesLT.CustomerAddress.AddressID = SalesLT.Address.AddressID
+where 
+  SalesLT.Address.City = 'Toronto';
 
 /*2. Сколько товаров со стоимостью (ListPrice) выше 1000 было продано?*/;
-SELECT 
-	COUNT(*) 
-FROM 
-	SalesLT.SalesOrderDetail 
-JOIN 
-	SalesLT.Product 
-ON	
-	SalesLT.SalesOrderDetail.ProductID = SalesLT.Product.ProductID 
-WHERE 
-	SalesLT.Product.ListPrice > 1000
+select 
+  count(*) 
+from 
+  SalesLT.SalesOrderDetail 
+  join SalesLT.Product on SalesLT.SalesOrderDetail.ProductID = SalesLT.Product.ProductID 
+where 
+  SalesLT.Product.ListPrice > 1000
 
 /*3. Отобразить названия организаций, суммарные покупки которых (включая налоги), превысили 50000.*/
-SELECT 
-	SalesLT.Customer.CompanyName, 
-	SalesLT.SalesOrderHeader.TotalDue 
-FROM 
-	SalesLT.SalesOrderHeader 
-JOIN 
-	SalesLT.Customer 
-ON 
-	SalesLT.SalesOrderHeader.CustomerID = SalesLT.Customer.CustomerID 
-WHERE 
-	SalesLT.SalesOrderHeader.TotalDue > 50000
+select 
+  SalesLT.Customer.CompanyName, 
+  SalesLT.SalesOrderHeader.TotalDue 
+from 
+  SalesLT.SalesOrderHeader 
+  join SalesLT.Customer on SalesLT.SalesOrderHeader.CustomerID = SalesLT.Customer.CustomerID 
+where
+  SalesLT.SalesOrderHeader.TotalDue > 50000
 
 /*4. Какие компании заказывали продукт (ProductModel) "Racing Socks"?*/
-SELECT 
-	SalesLT.Customer.CompanyName 
-FROM 
-	SalesLT.SalesOrderHeader 
-JOIN 
-	SalesLT.SalesOrderDetail 
-ON 
-	SalesLT.SalesOrderHeader.SalesOrderID = SalesLT.SalesOrderDetail.SalesOrderID
-JOIN   
-	(SELECT SalesLT.Product.ProductID, SalesLT.ProductModel.Name 
-	FROM SalesLT.Product 
-	JOIN SalesLT.ProductModel 
-	ON SalesLT.Product.ProductModelID = SalesLT.ProductModel.ProductModelID
-	WHERE SalesLT.ProductModel.Name = 'Racing Socks') AS Product 
-ON 
-	SalesLT.SalesOrderDetail.ProductID = Product.ProductID
-JOIN
-	SalesLT.Customer
-ON 
-	SalesLT.SalesOrderHeader.CustomerID = SalesLT.Customer.CustomerID
+select 
+  SalesLT.Customer.CompanyName 
+from 
+  SalesLT.SalesOrderHeader 
+  join SalesLT.SalesOrderDetail on SalesLT.SalesOrderHeader.SalesOrderID = SalesLT.SalesOrderDetail.SalesOrderID
+  join 
+    (select 
+       SalesLT.Product.ProductID, 
+	   SalesLT.ProductModel.Name 
+     from 
+       SalesLT.Product 
+       join SalesLT.ProductModel on SalesLT.Product.ProductModelID = SalesLT.ProductModel.ProductModelID
+     where 
+       SalesLT.ProductModel.Name = 'Racing Socks') as Product on SalesLT.SalesOrderDetail.ProductID = Product.ProductID
+  join SalesLT.Customer on SalesLT.SalesOrderHeader.CustomerID = SalesLT.Customer.CustomerID
 
 /*5. Отобразить 25 товаров с наибольшим суммарным чеком (количество * стоимость товара)*/
-SELECT 
-	TOP(25)
-	SalesLT.Product.ProductID,
-	SalesLT.Product.Name,
-	SalesLT.SalesOrderDetail.OrderQty * SalesLT.SalesOrderDetail.UnitPrice AS Total
-FROM 
-	SalesLT.SalesOrderDetail
-JOIN
-	SalesLT.Product
-ON
-	SalesLT.Product.ProductID = SalesLT.SalesOrderDetail.ProductID
-ORDER BY 
-	SalesLT.SalesOrderDetail.OrderQty * SalesLT.SalesOrderDetail.UnitPrice
-DESC
+select 
+  top(25)
+  SalesLT.Product.ProductID,
+  SalesLT.Product.Name,
+  SalesLT.SalesOrderDetail.OrderQty * SalesLT.SalesOrderDetail.UnitPrice as Total
+from 
+  SalesLT.SalesOrderDetail
+  join SalesLT.Product on SalesLT.Product.ProductID = SalesLT.SalesOrderDetail.ProductID
+order by
+  SalesLT.SalesOrderDetail.OrderQty * SalesLT.SalesOrderDetail.UnitPrice
+desc
 
 /*6. Сгруппировать заказы по диапазону стоимости: 0..99, 100..999, 1000..9999, свыше 10000.
 Для каждого диапазона отобразить количество заказов и общую стоимость.*/
-SELECT 
-	CASE WHEN SalesLT.SalesOrderDetail.LineTotal <= 99 THEN '0..99'
-		 WHEN SalesLT.SalesOrderDetail.LineTotal <= 999 THEN '100..999'
-		 WHEN SalesLT.SalesOrderDetail.LineTotal <= 9999 THEN '1000..9999'
-		 ELSE 'More 10000'
-	END AS Total,
-	COUNT (*) AS Count
-FROM
-	SalesLT.SalesOrderDetail
-GROUP BY
-	CASE WHEN SalesLT.SalesOrderDetail.LineTotal <= 99 THEN '0..99'
-		 WHEN SalesLT.SalesOrderDetail.LineTotal <= 999 THEN '100..999'
-		 WHEN SalesLT.SalesOrderDetail.LineTotal <= 9999 THEN '1000..9999'
-		 ELSE 'More 10000'
-	END
+select 
+  case when SalesLT.SalesOrderDetail.LineTotal <= 99 then '0..99'
+	   when SalesLT.SalesOrderDetail.LineTotal <= 999 then '100..999'
+	   when SalesLT.SalesOrderDetail.LineTotal <= 9999 then '1000..9999'
+	   else 'More 10000'
+  end as Total,
+  count (*) as Count
+from
+  SalesLT.SalesOrderDetail
+group by
+  case when SalesLT.SalesOrderDetail.LineTotal <= 99 then '0..99'
+	   when SalesLT.SalesOrderDetail.LineTotal <= 999 then '100..999'
+	   when SalesLT.SalesOrderDetail.LineTotal <= 9999 then '1000..9999'
+	   else 'More 10000'
+  end
 	
 /*7. Отобразить список компаний, содержащий 'bike' или 'cycle' в названии. 
 Отсортировать выборку так, чтобы сначала отображались компании с 'bike', а затем с 'cycle.*/
-SELECT 
-	CompanyName
-FROM 
-	SalesLT.Customer 
-WHERE 
-	SalesLT.Customer.CompanyName LIKE '%bike%' OR SalesLT.Customer.CompanyName LIKE '%cycle%'
-ORDER BY
-	CHARINDEX('cycle', SalesLT.Customer.CompanyName)
+select 
+  CompanyName
+from 
+  SalesLT.Customer 
+where 
+  SalesLT.Customer.CompanyName like '%bike%' or SalesLT.Customer.CompanyName like '%cycle%'
+order by
+  charindex('cycle', SalesLT.Customer.CompanyName)
 
 /*8. Отобразить 10 наиболее важных для продаж городов.*/
-SELECT 
-	TOP(10) 
-	SalesLT.Address.City,
-	SalesLT.SalesOrderHeader.TotalDue
-FROM 
-	SalesLT.SalesOrderHeader 
-JOIN 
-	SalesLT.CustomerAddress 
-ON 
-	SalesLT.SalesOrderHeader.CustomerID = SalesLT.CustomerAddress.CustomerID 
-JOIN 
-	SalesLT.Address 
-ON 
-	SalesLT.CustomerAddress.AddressID = SalesLT.Address.AddressID 
-ORDER BY 
-	SalesLT.SalesOrderHeader.TotalDue 
-DESC;
+select 
+  top(10) 
+  SalesLT.Address.City,
+  SalesLT.SalesOrderHeader.TotalDue
+from 
+  SalesLT.SalesOrderHeader 
+  join SalesLT.CustomerAddress on SalesLT.SalesOrderHeader.CustomerID = SalesLT.CustomerAddress.CustomerID 
+  join SalesLT.Address on SalesLT.CustomerAddress.AddressID = SalesLT.Address.AddressID 
+order by 
+  SalesLT.SalesOrderHeader.TotalDue 
+desc;
